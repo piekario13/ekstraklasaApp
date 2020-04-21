e41ceeb7db2e145306127724f7080bdca655b64e
piekario13 <piekario13@gmail.com>
2020-04-21 22:09:40 +0200

    add list

--- src/components/League.js
+++ src/components/League.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from "react";
+import "../styles/League.css";
 
-const League = ({ matches }) => matches.map((match, id) => (<li key={id}>
-    {match.team1.name} - {match.team2.name}
-    &nbsp;Wynik: {match.team1.score}:{match.team2.score}
-</li>))
+const League = ({ matches }) =>
+  matches.map((match, id) => (
+    <li key={id}>
+      {match.team1.name} - {match.team2.name} {match.team1.score}:
+      {match.team2.score}
+    </li>
+  ));
 
 export default League;
--- src/pages/Timetable.js
+++ src/pages/Timetable.js
@@ -1,31 +1,43 @@
-import React, { Fragment, useState, useEffect } from 'react'
-import '../styles/MainSubject.css'
-import League from '../components/League'
+import React, { Fragment, useState, useEffect } from "react";
+import "../styles/MainSubject.css";
+import League from "../components/League";
 
 const Timetable = () => {
-  const [rounds, setRounds] = useState([])
-  const [procesing, setProcesing] = useState(true)
+  const [rounds, setRounds] = useState([]);
+  const [procesing, setProcesing] = useState(true);
   useEffect(() => {
-    fetch('data/league.json')
-      .then(response => response.json())
-      .then(data => {
-        setRounds(data.rounds)
-        setProcesing(false)
-      })
-  }, [])
+    fetch("data/league.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setRounds(data.rounds);
+        setProcesing(false);
+      });
+  }, []);
   return !procesing ? (
     <ul>
       {rounds.map((element, id) => (
-        <Fragment key={id} >
-          <b>Kolejka: {element.name}</b> <br />
-          <League key={element.name} name={element.name} matches={element.matches} />
+        <Fragment key={id}>
+          <h1
+            style={{
+              backgroundColor: "#000",
+              color: "#fff",
+              textAlign: "center",
+            }}
+          >
+            {element.name}
+          </h1>{" "}
+          <br />
+          <League
+            key={element.name}
+            name={element.name}
+            matches={element.matches}
+          />
         </Fragment>
-      ))
-      }
+      ))}
     </ul>
   ) : (
-      'Trwa ładowanie'
-    )
-}
+    "Trwa ładowanie"
+  );
+};
 
 export default Timetable;
